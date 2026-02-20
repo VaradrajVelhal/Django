a107fe64e6552156521465507dd4784cf5d03878
Varadraj Velhal <varadrajvelhal@Varadrajs-MacBook-Air.local>
2026-02-20 20:26:58 +0530

    Learned about database relationships and django forms

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chai',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+TAILWIND_APP_NAME = 'theme' 
+
+INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIN_PATH = '/opt/homebrew/bin/npm'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -116,3 +127,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+
+print("MEDIA ROOT IS:", MEDIA_ROOT)
